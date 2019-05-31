4b948f9c2b7bf85caad7adb099dd1770c913d577
Camerian <camerianm@gmail.com>
2019-05-30 20:28:01 -0700

    Removed core radius error troubleshooting structures

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ startline=1000 #This is where the core stops and the mantle begins, in that file
 exo_composition=fromexo.bulk_mass_fraction(file,startline)
 composition = exo_composition
 
-''' # Build-your-own-planet mode:
+'''
+# Build-your-own-planet mode:
 composition = {'C2/c':5.605938492, 'Wus':0.196424301, 'Pv':58.03824705, 'an':0.00, \
                'O':0.249338793, 'Wad':0.072264906, 'Ring':0.028707673, 'Opx':14.88882685, \
                'Cpx':1.099284717, 'Aki':0.0000703828958617, 'Gt_maj':9.763623743, 'Ppv':6.440039009, \
                'CF':0.00, 'st':0.00, 'q':0.00, 'ca-pv':3.617239801, \
                'cfs':0.00, 'coe':0.00, 'ky':0.00, 'seif':0.00}
-'''
 
+'''
 method='dynamic'    #static, dynamic, or benchmark thermal parameters
 Mpl=1.0            #Planet mass in Me - usually between 0.5 and 5     Earth = 1.0
 Rpl=1.0             #Relative heat production per kg mantle, vs Earth  Earth = 1.0
@@ -37,10 +38,8 @@ Tp0=2000          #starting mantle potential temperature in K        Earth = 200
 tmax=4.55           #ending time, in Ga                                Earth=4.55
 Qp=1.0
 
+# Make sure your planet is self-consistent
 composition=get.adds_up(composition)
-CMF = get.CMF_estimate(Mpl,Rpl)
-try_CRF = get.CRF_estimate(Mpl,CMF,Rpl)
-
 
 # Build your mantle and acquire its unchanging material properties.
 Mp,Mc,Rp,Rc,d,Vm,Sa,pm,g,Pcmb,Tcmb=get.build(Mpl=Mpl,Rpl=Rpl,Tp0=Tp0)
@@ -53,8 +52,6 @@ params={"Mp":Mp,"Mc":Mc,"CMF":Mc/Mp,\
         "c1":c1,"Ev":Ev,"visc0":visc0}
 
 prnt.unchanging(params)
-
-
 
 #Evolve your planet over time
 Tp=Tp0
@@ -85,11 +82,13 @@ while t <= tmax:
 
 Evolution=np.asarray(Hts)
 
-print(Pf(t), '\t', Pf(Tp), '\t', Pe(Ra), '\t',  Pe(production), '\t', Pe(loss),  '\t', Pf(production/loss))
+print(Pf(t), '\t', Pf(Tp-273.15), '\t', Pe(Ra), '\t',  Pe(production), '\t', Pe(loss),  '\t', Pf(production/loss))
 print()
 
 Temps=evolve.plot_heat(Evolution[:,(0,1)],"Temperature (C) vs Time (Ga)")
+print('alpha=',alpha,'\tCp=',cp)
 
+'''
 
 #Compare approaches to calculating Cp and alpha
 methods=('dynamic','static','benchmark')    #static or dynamic thermal parameters
@@ -127,12 +126,12 @@ for method in methods:
         Tp=Tp+dTp
         t=t+dt
         
-    print(method, '  \t', Pf(Tp), '\t', Pe(Ra), '\t',  Pe(loss),  '\t', Pf(production/loss))
+    print(method, '  \t', Pf(Tp-273.15), '\t', Pe(Ra), '\t',  Pe(loss),  '\t', Pf(production/loss))
 
 Evolution=np.asarray(Hts)
 
 Temps=evolve.plot_heat(Evolution[:,(0,1)],"Temperature (C) vs Time (Ga)")
-
+print('alpha=',alpha,'\tCp=',cp)
 
 
 
@@ -172,14 +171,16 @@ for m in minerals:
 
         Tp=Tp+dTp
         t=t+dt
-    
-    print(m, '\t', Pf(Tp), '\t', Pe(Ra), '\t',  Pe(loss),  '\t', Pf(production/loss))
+    print('alpha=',alpha,'\tCp=',cp)
+
+    print(m, '\t', Pf(Tp-273.15), '\t', Pe(Ra), '\t',  Pe(loss),  '\t', Pf(production/loss))
 
 Evolution=np.asarray(Hts)
 
 Temps=evolve.plot_heat(Evolution[:,(0,1)],"Temperature (C) vs Time (Ga)")
-Rayleighs=evolve.plot_heat(Evolution[:,(0,2)],"Rayleigh number vs Time (Ga)")
-Heat_loss=evolve.plot_heat(Evolution[:,(0,4)],"Heat loss (W) vs Time (Ga)")
+#Rayleighs=evolve.plot_heat(Evolution[:,(0,2)],"Rayleigh number vs Time (Ga)")
+#Heat_loss=evolve.plot_heat(Evolution[:,(0,4)],"Heat loss (W) vs Time (Ga)")
 #Heat_production=evolve.plot_heat(Evolution[:,(0,3)],"Heat production (W) vs Time (Ga)")
+#plt.savefig('radiogenic.pdf')#Rayleighs=evolve.plot_heat(Evolution[:,(0,2)],"Rayleigh number vs Time (Ga)")
 
-
+'''
